18ed9fca793ae4e176bfdc30fafd219fe8a10409
Sofiia-Solomiia <sofiia-solomiia.pavlyshyn.mitpa.2024@lpnu.ua>
2025-05-21 09:20:42 +0300

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,23 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public.id
+}
+
+output "private_subnet_id" {
+  value = aws_subnet.private.id
+}
+
+output "web_server_public_ip" {
+  value = aws_instance.web.public_ip
+}
+
+output "web_server_public_dns" {
+  value = aws_instance.web.public_dns
+}
+
+output "web_server_id" {
+  value = aws_instance.web.id
+}
