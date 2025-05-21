531b1dcf8a9f2565545d34c1eff886dc3195e2d1
Sofiia-Solomiia <sofiia-solomiia.pavlyshyn.mitpa.2024@lpnu.ua>
2025-05-21 09:17:46 +0300

    Create instances.tf

---
+++ instances.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.web_server.id]
+  key_name               = aws_key_pair.deployer.key_name
+  user_data              = file("userdata.tpl")
+
+  tags = {
+    Name = "${var.project_name}web-server"
+  }
+}
