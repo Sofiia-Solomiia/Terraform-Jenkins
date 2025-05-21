ed0ad4cc422d9057cc8ac29b567e6f4ce9f24cc1
Sofiia-Solomiia <sofiia-solomiia.pavlyshyn.mitpa.2024@lpnu.ua>
2025-05-21 09:11:12 +0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "aws_region" {
+  default = "eu-north-1"
+}
+
+variable "project_name" {
+  default = "lab6-terraform"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+  default = "10.0.2.0/24"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "public_key_path" {
+  type    = string
+  default = "tests.pub"
+}
