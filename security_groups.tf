4f48c98a0d6919052094802606cf5ab721cc4834
Sofiia-Solomiia <sofiia-solomiia.pavlyshyn.mitpa.2024@lpnu.ua>
2025-05-24 17:38:16 +0300

    Update security_groups.tf

--- security_groups.tf
+++ security_groups.tf
@@ -35,36 +35,3 @@ resource "aws_security_group" "web_server" {
     Name = "${var.project_name}-web-sg"
   }
 }
-resource "aws_security_group" "monitoring_sg" {
-  name        = "${var.project_name}-monitoring-sg"
-  description = "Allow access to Prometheus and Grafana"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    from_port   = 9090
-    to_port     = 9090
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Prometheus
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Grafana
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
