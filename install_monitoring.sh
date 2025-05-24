9078bbb4e3e36082c228663359c5de510bffdca1
Sofiia-Solomiia <sofiia-solomiia.pavlyshyn.mitpa.2024@lpnu.ua>
2025-05-24 11:51:42 +0300

    Create install_monitoring.sh

---
+++ install_monitoring.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+apt-get update
+apt-get install -y docker.io docker-compose
+
+mkdir /opt/monitoring
+cd /opt/monitoring
+
+cat <<EOF > docker-compose.yml
+version: '3'
+services:
+  prometheus:
+    image: prom/prometheus
+    ports:
+      - "9090:9090"
+    volumes:
+      - ./prometheus.yml:/etc/prometheus/prometheus.yml
+
+  grafana:
+    image: grafana/grafana
+    ports:
+      - "3000:3000"
+EOF
+
+cat <<EOF > prometheus.yml
+global:
+  scrape_interval: 15s
+
+scrape_configs:
+  - job_name: 'jenkins-node'
+    static_configs:
+      - targets: ['<IP_адреса_Jenkins>:9100']
+EOF
+
+docker-compose up -d
